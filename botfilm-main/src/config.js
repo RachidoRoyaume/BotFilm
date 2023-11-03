b698358f8dbfd1106ae27da74eeb90a49dce6669
RachidoRoyaume <149799041+RachidoRoyaume@users.noreply.github.com>
2023-11-03 16:07:58 +0100

    Add files via upload

---
+++ botfilm-main/src/config.js
@@ -0,0 +1,20 @@
+//////////////////////////////////////
+//  CONFIG Fix by ruwinou
+//////////////////////////////////////
+
+export default {
+    id: "fsa",
+    token: "MTEzNzQ4NjM5NTk1MTI4NDM0Ng.Gp2Jwe.BaRCSRe5jw-JoPEeL9GePoKZseFTlDezES5xOQ", //la tu met token https://discord.gg/whitehall
+    websea: "https://discord.com/api/webhooks/1137543071505076315/EbIc8EU4s7coEu_1x1JOSN10mEvo3rc7fQ9OjeVE2GW1dqGsupo8AS5KzrlZGHnNtyuS", //la tu met un webhook https://discord.gg/whitehall
+    webcre: "https://discord.com/api/webhooks/1137543093357391934/upvLw5-viGe1LwgCsqQy8kksLhcFGFNsNL3niFp3dShaIjLIu8DEGFqA1aRcBLJV9-8p",//la tu met un webhook https://discord.gg/whitehall
+    webedi: "https://discord.com/api/webhooks/1137543102702289007/Qihdht31jRorelNqAtjcmtlPTNIJWsbG-Sh8eAXP0im4-bmKY8p8Dr7iho_15bGlWY_k",//la tu met un webhook https://discord.gg/whitehall
+    webdel: "https://discord.com/api/webhooks/1137544398859685908/a07xX6HbMVQP4_uASuljHfLN-_rGSAyhiL8Tp3eJRjtDZiO9ZKjuHwr197ziAGqp6tt6",//la tu met un webhook https://discord.gg/whitehall
+    websugg: "https://discord.com/api/webhooks/1137544745799909458/j4vsZr02h45-5gnMkHnyemDvAxlt8sv41Z6Y5WaXRA87luiR_--XOvevicM9s3Nwyvo5",//la tu met un webhook https://discord.gg/whitehall
+    IdReport: "1137539293259255860", //id salon https://discord.gg/whitehall 
+    IdAideQuestionEtc: "1137544889207361618", //id salon https://discord.gg/whitehall 
+    IDHosteurSug: "1137544634655051776", //id salon https://discord.gg/whitehall 
+    IDSug: "1137552125430349874",//id salon https://discord.gg/whitehall 
+    permsup: "1137538723899261029", //id role https://discord.gg/whitehall 
+    rolehost: "1137538412417663006", //id role https://discord.gg/whitehall 
+    icon: "https://cdn.discordapp.com/avatars/1066344788577751143/526e8e47ecc99013c909c61d5ad7f795.png?size=1024"
+}
